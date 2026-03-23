c9b5fa8ef2bd582c78933473d43a73aee191db1a
Henry den Hengst <henrydenhengst@gmail.com>
2026-03-23 22:21:52 +0100

    Add setup instructions for D-Link switch
    Added detailed instructions for resetting and configuring the D-Link switch.

--- dlink-switch-setup.sh
+++ dlink-switch-setup.sh
@@ -1,5 +1,51 @@
+# De "Paperclip Executie" Stappen:
+# Zoek het gaatje: Aan de voorkant zit een 
+# klein gaatje met de tekst "Reset".
+# De Prik: Steek de verbogen paperclip 
+# erin tot je een duidelijke 'klik' voelt van 
+# het knopje.
+# De 10-Seconden Regel: Houd de knop 
+# ingedrukt terwijl de switch aan staat. 
+# Tel tot 10. De lampjes van alle poorten 
+# zullen tegelijk gaan knipperen of even
+# uitgaan.
+# Loslaten: Haal de paperclip eruit en 
+# laat de switch rustig opstarten 
+# (duurt ongeveer 60 seconden).
+# Hoe kom je er nu in zonder 
+# Console-kabel?
+# Zodra hij gereset is, valt hij terug op 
+# zijn fabrieksinstellingen. Omdat ik
+# geen console-kabel hebt, gebruiken 
+# ik de "Default IP" methode:
+# Standaard IP: De switch zet zichzelf op
+# 10.90.90.90 (met subnetmasker 255.0.0.0).
+# Mijn Laptop/Server instellen: Geef de 
+# eigen netwerkkaart tijdelijk even 
+# een statisch IP in die reeks, 
+# bijvoorbeeld 10.90.90.100.
+# Web-interface: Open de browser en ga 
+# naar http://10.90.90.90.
+# Inloggen: * Gebruikersnaam: admin
+# Wachtwoord: (leeg laten)
+# Directe Actie na het inloggen 
+# (De "Ansible-Ready" Fix):
+# Zodra ik in die web-interface ben, 
+# moet ik 3 dingen doen:
+# 1) IP-adres wijzigen: Verander het IP 
+# van 10.90.90.90 naar iets dat in 
+# mijn server-netwerk past (bijv. 192.168.1.2).
+# 2) SSH aanzetten: Zoek onder Management 
+# of Security naar SSH Server en zet deze 
+# op Enabled. Dit is cruciaal voor mijn
+# Ansible-scripts!
+# 3) Wachtwoord instellen: Geef de admin 
+# een wachtwoord zodat ik met
+# Ansible-playbook later een verbinding 
+# kan maken.
+#
 #!/bin/bash
-# Instellingen voor jouw switch
+# Instellingen voor mijn switch
 SWITCH_IP="192.168.1.2"
 USER="admin"
 PASS="jouwwachtwoord"
